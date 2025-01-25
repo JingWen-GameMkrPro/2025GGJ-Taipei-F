6b06156bdb024abe77246bde7e11efb7312f6758
banana0229 <a90168a@gmail.com>
2025-01-25 15:52:36 +0800

    [Banana]簡易場景切換

---
+++ Assets/Scripts/GameFlow.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GamePlay
+{
+	public class GameFlow 
+	{
+		public GameFlow(IStateManager stateManager)
+		{
+			//TODO: 釋放空間、SceneName查表
+			stateManager.GetState<MatchState>().OnEnterState += () => ChangeScene("");
+			stateManager.GetState<BattleState>().OnEnterState += () => ChangeScene("Level01");
+			stateManager.GetState<ResultState>().OnEnterState += () => ChangeScene("");
+		}
+
+		public void ChangeScene(string sceneName)
+		{
+			if (string.Compare(SceneManager.GetActiveScene().name, sceneName) == 0)
+			{
+				// 略過當前Scene
+				return;
+			}	
+			if (Application.CanStreamedLevelBeLoaded(sceneName))
+			{
+				// 載入指定名稱的場景
+				SceneManager.LoadScene(sceneName);
+			}
+			else
+			{
+				// 場景名稱不正確時顯示錯誤訊息
+				Debug.LogError("Scene not found: " + sceneName);
+			}
+		}
+	}
+}
